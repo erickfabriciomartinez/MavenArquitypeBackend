613f59bba3804f65899364a26b983d912623fa2d
Erick Martínez <erickfabriciomartinez@gmail.com>
2019-05-15 18:02:25 -0600

    Estructura principal de proyecto

---
+++ src/main/java/hn/efmc/main/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 Erick Fabricio Martínez Castellanos.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hn.efmc.main;
+
+/**
+ * Clase principal de aplicación.
+ * @author Erick Fabricio Martínez Castellanos
+ * @version 1.0.0 15-may-2019
+ */
+public class Main {
+
+    /**
+     * Inicio de aplicación
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hn.efmc");
+    }
+
+}
